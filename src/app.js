fd341cf209b80c29ef2fad4641583556e96e3ad9
pedromax23 <pedromorgade1234@gmail.com>
2024-06-07 17:09:52 -0300

    Volviendo a los cambios

--- src/app.js
+++ src/app.js
@@ -6,47 +6,37 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import { testConnection } from './db.js';
 import { ORIGIN } from './config.js';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-// Definir __dirname para módulos ES
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 
 // Middlewares
 app.use(cors({
-    origin: ORIGIN, // Las direcciones que pueden pedir datos a esta API
-    credentials: true // Porque se envían datos por cookies
+    origin: ORIGIN, // Las direcciones que pueden pedir datos a esta api
+    credentials: true // Por que se envian datos por cookies 
 }));
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Rutas de la API
+
+// Routes
+app.get('/', (req, res) => res.json({ message: "Welcome to my API" }));
 app.get('/api/ping', async (req, res) => {
-    await testConnection();
-    res.json({ message: 'pong' });
+    await testConnection()
+    res.json({
+        message: 'Pong'
+    })
 });
-app.use('/api', taskRoutes);
-app.use('/api', authRoutes);
+app.use('/api', taskRoutes)
+app.use('/api', authRoutes)
 
-// Servir archivos estáticos del directorio "frontend/build"
-app.use(express.static(path.join(__dirname, '..', 'frontend', 'build')));
-
-// Capturar todas las demás rutas y devolver index.html
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'frontend', 'build', 'index.html'));
-});
-
-// Manejador de errores
+// Error Handler
 app.use((err, req, res, next) => {
     res.status(500).json({
         status: "error",
         message: err.message
-    });
+    })
 });
 
 export default app;
